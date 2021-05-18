41757a9957b8fba662bcfb68cfccf8e5731e9006
niamulhasan <niamulhasanbd@gmail.com>
2021-05-18 07:26:28 +0600

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+function showCart() {
+    const collapseCart = document.getElementById("collapseCart");
+    collapseCart.style.display = "block";
+    collapseCart.classList.add("shader-animate");
+}
+
+function hideCart() {
+    const collapseCart = document.getElementById("collapseCart");
+    collapseCart.style.display = "none";
+}
